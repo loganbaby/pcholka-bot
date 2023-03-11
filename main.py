119a46044e9da3c0d57551ebf12213aba6d420df
logbaby <96885996+loganbaby@users.noreply.github.com>
2023-03-11 12:15:18 +0300

    Update main.py for termux)))

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from account import time_avatar_on, time_avatar_off, display_help
 
 if __name__ == '__main__':
     app.add_handler(MessageHandler(search_system_filter))  # search info in google (3 pages)
-    app.add_handler(MessageHandler(get_popular_capitalization))  # get capitalization of popular coins
-    app.add_handler(MessageHandler(get_all_crypto_coins))  # get list of available crypto coins
-    app.add_handler(MessageHandler(get_crypto_dynamic))  # get crypto dynamic by count of the days
     app.add_handler(MessageHandler(message_spam))  # message spam (count)
     app.add_handler(MessageHandler(reaction_spam))  # reaction spam (all chat)
     app.add_handler(MessageHandler(time_avatar_on))  # avatar display current time
